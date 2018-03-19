a85f36c7673d8a7c6bf754c3091a19475ae3e9e8
Athurg Feng <athurg@gooth.org>
2018-03-19 15:05:53 +0800

    添加“域名解析记录”添加的功能

--- record.go
+++ record.go
@@ -2,6 +2,7 @@ package cns
 
 import (
 	"net/url"
+	"strconv"
 )
 
 //“域名”“解析记录”的数据结构
@@ -54,4 +55,48 @@ func (cli *Client) RecordList(domain string) ([]Record, error) {
 	}
 
 	return respInfo.Data.Records, nil
+}
+
+//添加指定“域名”的“解析记录”
+func (cli *Client) RecordCreate(domain string, record Record) (int, error) {
+	if record.Line == "" {
+		record.Line = "默认"
+	}
+
+	//必选参数
+	param := url.Values{
+		"domain":     {domain},
+		"subDomain":  {record.Name},
+		"recordType": {record.Type},
+		"recordLine": {record.Line},
+		"value":      {record.Value},
+	}
+
+	//可选TTL参数，缺省为600
+	if record.Ttl > 0 {
+		param.Set("ttl", strconv.Itoa(record.Ttl))
+	}
+
+	//MX记录必须的额外参数
+	if record.Type == "MX" {
+		param.Set("mx", strconv.Itoa(record.Mx))
+	}
+
+	var respInfo struct {
+		BaseResponse
+		Data struct {
+			Record struct {
+				Id     int `json:",string"`
+				Name   string
+				Status string
+				Weight interface{}
+			}
+		}
+	}
+	err := cli.requestGET("RecordCreate", param, &respInfo)
+	if err != nil {
+		return 0, err
+	}
+
+	return respInfo.Data.Record.Id, nil
 }
